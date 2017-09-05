89d82ffbe2e1b9072a9da31d61a2a71f14a5fba0
Dominic Valenciana <kiricon@live.com>
2017-09-04 19:50:28 -0600

    tabs now wait for child elements

--- fun-tabs.js
+++ fun-tabs.js
@@ -10,20 +10,6 @@ template.innerHTML = `
             display: table;
         }
 
-        ::slotted(fun-tab) {
-            opacity: 0.6;
-            transition: opacity ease 0.3s;
-            cursor: pointer;
-            padding: 10px 15px;
-            display: inline-block;
-            text-align: center;
-        }
-
-        ::slotted(fun-tab:hover), ::slotted(fun-tab.selected) {
-            opacity: 1;
-        }
-
-
         div {
             height: 2px;
             background-color: var(--fun-tabs-color, var(--secondary-color, #673AB7));
@@ -79,16 +65,26 @@ class FunTabs extends HTMLElement {
         
         let initialIndex = this.getAttribute('selected') || 0;
 
-        this.tabWidth = this.offsetWidth / tabs.length;
-        this.div = this.shadowRoot.querySelector('div');
-        this.div.style.marginLeft = `${this.tabWidth*initialIndex}px`
-        this.div.style.width = `${this.tabWidth}px`;
         window.requestAnimationFrame(() => {
+            this.tabWidth = this.offsetWidth / tabs.length;
+            this.div = this.shadowRoot.querySelector('div');
+            this.div.style.marginLeft = `${this.tabWidth*initialIndex}px`
+            this.div.style.width = `${this.tabWidth}px`;
             this.div.style.transition = 'ease all 0.15s';
+            this.setSelectedTab(initialIndex);
+            this.addEventListener('resize', this.adjustWidth);
         });
         
-        
-        this.setSelectedTab(initialIndex);
+    }
+
+    adjustWidth() {
+        let tabs = this.querySelectorAll('fun-tab');
+        let tabIndex = this.getAttribute('selected');
+        this.tabWidth = this.offsetWidth / tabs.length;
+        this.div = this.shadowRoot.querySelector('div');
+        this.div.style.marginLeft = `${this.tabWidth*tabIndex}px`;
+        this.div.style.width = `${this.tabWidth}px`;
+        this.setSelectedTab(tabIndex);
     }
 
     /**
@@ -158,4 +154,79 @@ class FunTabs extends HTMLElement {
 }
 
 customElements.define("fun-tabs", FunTabs);
+
+const tabTemplate = document.createElement("template");
+tabTemplate.innerHTML = `
+    <style>
+        :host {
+            display: table;
+            opacity: 0.6;
+            transition: opacity ease 0.3s;
+            cursor: pointer;
+            padding: 10px 15px;
+            display: inline-block;
+            text-align: center;
+        }
+
+
+        :host:hover, :host.selected {
+            opacity: 1;
+        }
+
+    </style>
+    <slot></slot>
+`;
+
+/**
+ * This is the class that controls each instance of your custom element.
+ */
+class FunTab extends HTMLElement {
+    /**
+     * Part of the custom element spec. Returns an array of strings that are 
+     * the names of attributes that this element observes/listens to.
+     * 
+     * @returns {Array} an array of strings, each of which representing an 
+     *  attribute.
+     */
+
+    constructor() {
+        super();
+
+        // create shadow root for any children context
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(tabTemplate.content.cloneNode(true));
+    }
+
+    /**
+     * Part of the custom element spec. Called after your element is attached to
+     * the DOM. Do anything related to the element or its children here in most
+     * cases.
+     */
+    connectedCallback() {
+    }
+
+    /**
+     * Part of the custom element spec. Called after your element is remove from
+     * the DOM. Disconnect any listeners or anything else here.
+     */
+    disconnectedCallback() {
+
+    }
+
+    /**
+     * Part of the custom element spec. Called when one of the observed
+     * attributes changes, either via setAttribute() or with the attribute being
+     * manually set in the HTML.
+     * 
+     * @param {String} name the name of the attribute that changed
+     * @param {Mixed} oldValue the previous value of the attribute
+     * @param {Mixed} newValue the new value of the attribute
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+        // respond to a changed attribute here
+    }
+
+}
+
+customElements.define("fun-tab", FunTab);
 })();
